4fe5edeaec03047802cde639303dff50141602b8
darielgaizta <darielgaz@gmail.com>
2023-11-01 09:46:44 +0700

    post feeds

---
+++ README.md
@@ -0,0 +1,2 @@
+Learning how to use Go for back-end while building an RSS Aggregator (project-based learning).
+> note: port and database url is required in a .env
--- models.go
+++ models.go
@@ -28,3 +28,33 @@ func serializeUser(dbUser database.User) User {
 		ApiKey:    dbUser.ApiKey,
 	}
 }
+
+// Feed
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
+}
+
+func serializeFeed(dbFeed database.Feed) Feed {
+	return Feed{
+		ID:        dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name:      dbFeed.Name,
+		Url:       dbFeed.Url,
+		UserID:    dbFeed.UserID,
+	}
+}
+
+func serializeManyFeeds(dbFeeds []database.Feed) []Feed {
+	feeds := []Feed{}
+	for _, dbFeed := range dbFeeds {
+		feeds = append(feeds, serializeFeed(dbFeed))
+	}
+	return feeds
+}
